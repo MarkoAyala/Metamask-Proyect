4e4de8acb34b8a28c4d088d9ccd49d072c9e0249
MarkoAyala <markoayala3@hotmail.com>
2022-07-01 21:30:02 -0300

    implementando tabla

--- meta-connect/src/App.js
+++ meta-connect/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import WalletCard from './WalletCard.js'
+import Table from './components/Table/Table';
+import axios from 'axios';
+import { useEffect , useState } from 'react';
 
 function App() {
+
+  let [coins , setCoins] = useState([]);
+
+
+  const getData = async ()=>{
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    setCoins(res.data);
+    console.log(coins)
+  }
+  useEffect(()=>{
+    getData()
+  } ,[])
   return (
     <div className="App">
       <WalletCard/>
+
+      <h1>Coin Market</h1>
+      <Table coins={coins}/>
     </div>
   );
 }
---
+++ meta-connect/src/components/Table/Table.js
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+const columns = [
+  { id: 'id', label: '#', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+];
+
+
+
+
+export default function TableCoin({coins}) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <div style={{width:"100%", display:"flex", justifyContent:"center"}}>
+
+    <Paper sx={{ width: '80%'}}>
+      <TableContainer sx={{ width:"100%"}}>
+        <Table stickyHeader aria-label="sticky table" sx={{ width:"100%"}}>
+          <TableHead sx={{ width:"100%"}}>
+            <TableRow sx={{ width:"100%"}}>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ width:"100%"}}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ width:"100%"}}>
+            {coins
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code} sx={{ width:"100%"}}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+
+                      if(column.id === "id"){
+                        return (
+                            <TableCell key={column.id} align={column.align}>
+                                {index +1}
+                            </TableCell>
+                        )
+                      }  
+
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={coins?.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+    </div>
+  );
+}
